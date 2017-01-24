83d0b4ee6e3fc12c4c4ad58306b14d7350013e05
rdelvek <r.delvek@gmail.com>
2017-01-24 10:45:57 -0500

    Create vscode_user_settings.js

---
+++ vscode_user_settings.js
@@ -0,0 +1,88 @@
+// VS Code User Settings: (make sure you install Guides plugin)
+
+// Place your settings in this file to overwrite the default settings
+{
+    "editor.fontFamily": "Source Code Pro, Inconsolata, Monaco, Consolas, 'Courier New', monospace",
+    "editor.fontSize": 14,
+    "editor.tabSize": 2,
+    "terminal.integrated.fontFamily": "Inconsolata, Monaco, Consolas, 'Courier New', monospace",
+    "terminal.integrated.fontSize": 12,
+    "window.zoomLevel": -1,
+    "typescript.check.tscVersion": false,
+    "files.exclude": {
+        "**/dist":true
+    },
+    "editor.renderWhitespace": true,
+    "editor.wordSeparators": "`~!@#$%^&*()-=+[{]}\\|;:'\",.<>/?",
+    "editor.roundedSelection": true,
+    "editor.glyphMargin": false,
+
+    "guides.indent.backgrounds": [],
+
+    // Hide indentation background in selections.
+    "guides.indent.hideBackgroundOnSelection": false,
+
+    // Show start-of-line indentation guides.
+    "guides.indent.showFirstIndentGuides": true,
+
+    // Normal indentation guides rendering width.
+    "guides.normal.width": 1,
+
+    // Normal indentation guides rendering color for dark themes.
+    "guides.normal.color.dark": "rgba(60, 60, 60, 0.75)",
+
+    // Normal indentation guides rendering color for light themes.
+    "guides.normal.color.light": "rgba(220, 220, 220, 0.75)",
+
+    // Normal indentation guides rendering style.
+    "guides.normal.style": "solid",
+
+    // Hide normal indentation guides in selections.
+    "guides.normal.hideOnSelection": false,
+
+    // Enable active indentation guides in addition to normal indentation guides.
+    "guides.active.enabled": true,
+
+    // Active indentation guides rendering width.
+    "guides.active.width": 1,
+
+    // Active indentation guides rendering color for dark themes.
+    "guides.active.color.dark": "rgba(200, 60, 60, 0.75)",
+
+    // Active indentation guides rendering color for light themes.
+    "guides.active.color.light": "rgba(200, 100, 100, 0.75)",
+
+    // Active indentation guides rendering style.
+    "guides.active.style": "solid",
+
+    // Hide active indentation guides in selections.
+    "guides.active.hideOnSelection": false,
+
+    // Enable stack indentation guides in addition to normal indentation guides.
+    "guides.stack.enabled": true,
+
+    // Stack indentation guides rendering width.
+    "guides.stack.width": 1,
+
+    // Stack indentation guides rendering color for dark themes.
+    "guides.stack.color.dark": "rgba(80, 80, 80, 0.75)",
+
+    // Stack indentation guides rendering color for light themes.
+    "guides.stack.color.light": "rgba(180, 180, 180, 0.75)",
+
+    // Stack indentation guides rendering style.
+    "guides.stack.style": "solid",
+
+    // Hide stack indentation guides in selections.
+    "guides.stack.hideOnSelection": false,
+
+    // Time duration between each guide lines update (in seconds).
+    "guides.updateDelay": 0.1,
+
+    // Override default Visual Studio Code behaviours (such as indentation guides or rulers).
+    //   Set to "true" to suppress any suggestion towards default Visual Studio Code behaviours.
+    "guides.overrideDefault": false,
+
+    // Send anonymous usage statistics data to the developer.
+    "guides.sendUsagesAndStats": true
+    }
